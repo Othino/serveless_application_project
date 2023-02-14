05787033b338d78f5facd688f56784378e2553a7
Othino <micahothino44@gmail.com>
2023-02-14 11:59:33 +0300

    final cloud dev project

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-3my4c7cefrxf54wy.us.auth0.com',
+  clientId: 'G7RWEyJWjSRyr11YQp5JA042LOO6qP1V',
+  callbackUrl: 'http://localhost:3000/callback'
+};
